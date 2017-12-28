e67132aeded45df90133c4c0e3c37a12f47f0bd2
Diogo Cezar <diogo@diogocezar.com>
2017-12-28 17:28:25 -0200

    Ajustes Gerais de Responsividade

--- js/src/objects/index.js
+++ js/src/objects/index.js
@@ -1,29 +1,63 @@
 Index = {
+	Menu: {
+		init: function() {
+            $(".go-menu").on("click", function(e) {
+                e.preventDefault();
+                var o = $(this);
+                $("html, body").stop().animate({
+                    scrollTop: $(o.attr("href")).offset().top
+                }, 1e3, "easeOutQuart")
+            })
+        }
+	},
 	Carousels: {
 		configs: {
 			'default' : {
 				loop               : true,
-				nav                : false,
-				pagination         : false,
+				nav                : true,
+				pagination         : true,
 				items              : 1,
-				dots               : false,
+				dots               : true,
 				autoplay           : false,
-				autoplayTimeout    : 3000,
+				autoplayTimeout    : 5000,
 				autoplayHoverPause : true,
 				singleItem         : true,
-				navText            : ["<i class='nav-left'></i>", "<i class='nav-right'></i>"]
+				navText            : ["<i class='arrow-prev'></i>","<i class='arrow-next'></i>"]
+			},
+			'agendamobile' : {
+				loop               : true,
+				nav                : true,
+				pagination         : true,
+				dots               : true,
+				autoplay           : false,
+				autoplayTimeout    : 5000,
+				autoplayHoverPause : true,
+				singleItem         : true,
+				navText            : ["<i class='arrow-prev'></i>","<i class='arrow-next'></i>"],
+				responsive:{
+				    0 : {
+				        items:1
+				    },
+				    768: {
+				    	items: 2
+				    },
+				    1024 : {
+				        items:3
+				    },
+				    1200 : {
+				        items:4
+				    }
+				}
 			}
 		},
 		init: function(){
 			$('.carousel-default').owlCarousel(Index.Carousels.configs['default']);
-			$('.carousel-default').on('mouseout', function(){
-				$('.carousel-default').trigger('stop.owl.autoplay');
-				$('.carousel-default').trigger('play.owl.autoplay', [3000]);
-			});
+			$('.carrousel-agenda-mobile').owlCarousel(Index.Carousels.configs['agendamobile']);
 		}
 	},
     init: function(){
-    	Index.Carousels.init();
+    	Index.Menu.init();
+		Index.Carousels.init();
     }
 }
 
